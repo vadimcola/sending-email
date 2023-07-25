84942442d62af52c48660bc7cec0ee46a014341b
Vadim Andreev <vadimcola@mail.ru>
2023-07-25 16:59:42 +0700

    Создано приложение mailing

--- mailing/models.py
+++ mailing/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Client(models.Model):
+    client_name = models.CharField(max_length=255, **NULLABLE, verbose_name='ФИО Клиента')
+    email = models.EmailField(unique=True, verbose_name='Почта')
+    comment = models.TextField(**NULLABLE, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.email}'
+
+
+class Message(models.Model):
+    subject_message = models.CharField(max_length=255, **NULLABLE,
+                                       verbose_name='Тема сообщения')
+    message = models.TextField(**NULLABLE, verbose_name='Сообщение')
+    mailing_time = models.DateTimeField(verbose_name='Время отправки')
+
