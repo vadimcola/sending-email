3ad483b7f719dc9d62652e199657c830b1dba3d4
Vadim Andreev <vadimcola@mail.ru>
2023-07-26 17:32:03 +0700

    Реализованна моодель Message и Setting

--- mailing/models.py
+++ mailing/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 NULLABLE = {'blank': True, 'null': True}
 
@@ -16,5 +17,28 @@ class Message(models.Model):
     subject_message = models.CharField(max_length=255, **NULLABLE,
                                        verbose_name='Тема сообщения')
     message = models.TextField(**NULLABLE, verbose_name='Сообщение')
-    mailing_time = models.DateTimeField(verbose_name='Время отправки')
 
+    def __str__(self):
+        return f'{self.subject_message}'
+
+
+class Setting(models.Model):
+
+    FREQUENCY = [
+        (None, 'Не указано'),
+        ('OPD', 'Раз в день'),
+        ('OPW', 'Раз в неделю'),
+        ('OPM', 'Раз в месяц')
+    ]
+
+    STATUS = [
+        (None, 'Не указано'),
+        ('created', 'Создана'),
+        ('active', 'Запущена'),
+        ('closed', 'Завершена')
+    ]
+    mailing_time = models.DateTimeField(default=timezone.now, verbose_name='Время отправки')
+    frequency_mailing = models.CharField(max_length=3, choices=FREQUENCY,
+                                         verbose_name='Переодичность отправки')
+    mailing_status = models.CharField(max_length=7, choices=STATUS,
+                                      verbose_name='Статус отправки')
